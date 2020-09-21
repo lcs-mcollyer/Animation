37714732550ddf899c44b78261384e012e818ca4
matt collyer <mcollyer@lcs.on.ca>
2020-09-21 10:02:12 -0400

    learned how to draw a grid of shapes

---
+++ Playground.playground/Pages/T-shirt.xcplaygroundpage/Contents.swift
@@ -0,0 +1,76 @@
+//: [Previous](@previous) / [Next](@next)
+//: # Blank canvas to copy
+//:
+//: ## To duplicate this page
+//:
+//: Place your cursor on line 19 of this page, press `Command-A` to select all text, and then `Command-C` to copy.
+//:
+//: Two-finger tap on the playground, then choose **New Playground Page**:
+//:
+//: ![new-playground](new-playground.png "New Playground")
+//:
+//: Move to the new page and press `Command-A` to select all text there, then `Command-V` to paste.
+//:
+/*:
+ ## Canvas size
+ 
+ Set the size of your desired canvas by adjusting the constants on lines 19 and 20.
+ */
+let preferredWidth = 300
+let preferredHeight = 600
+/*:
+ ## Required code
+ 
+ Lines 28 to 36 are required to make the playground run.
+ 
+ Please do not remove.
+ */
+import Cocoa
+import PlaygroundSupport
+import CanvasGraphics
+
+// Create canvas
+let canvas = Canvas(width: preferredWidth, height: preferredHeight)
+
+// Show the canvas in the playground's live view
+PlaygroundPage.current.liveView = canvas
+/*:
+ ## Add your code
+ 
+ Beginning on line 48, write a meaningful comment.
+ 
+ You can remove the code on line 49 and begin writing your own code.
+ 
+ [Documentation](http://russellgordon.ca/CanvasGraphics/Documentation/) is available.
+
+ */
+canvas.drawLine(from: Point(x: 150, y: 150), to: Point(x: 200, y: 175))
+canvas.drawLine(from: Point(x: 200, y: 175), to: Point(x: 150, y: 200))
+canvas.drawLine(from: Point(x: 150, y: 200), to: Point(x: 100, y: 175))
+canvas.drawLine(from: Point(x: 100, y: 175), to: Point(x: 150, y: 150))
+
+canvas.drawLine(from: Point(x: 150, y: 200), to: Point(x: 150, y: 250))
+canvas.drawLine(from: Point(x: 150, y: 250), to: Point(x: 200, y: 225))
+canvas.drawLine(from: Point(x: 200, y: 225), to: Point(x: 200, y: 175))
+
+canvas.drawLine(from: Point(x: 100, y: 175), to: Point(x: 100, y: 225))
+canvas.drawLine(from: Point(x: 100, y: 225), to: Point(x: 150, y: 250))
+
+canvas.drawLine(from: Point(x: 100, y: 225), to: Point(x: 50, y: 250))
+canvas.drawLine(from: Point(x:150 , y: 250), to: Point(x: 100, y: 275))
+canvas.drawLine(from: Point(x: 50, y: 250), to: Point(x: 100, y: 275))
+canvas.drawLine(from: Point(x: 150, y: 250), to: Point(x: 200, y: 275))
+canvas.drawLine(from: Point(x: 200, y: 275), to: Point(x: 250, y: 250))
+canvas.drawLine(from: Point(x: 250, y: 250), to: Point(x: 200, y: 225))
+canvas.drawLine(from: Point(x: 150, y: 250), to: Point(x: 150, y: 300))
+canvas.drawLine(from: Point(x: 150, y: 300), to: Point(x: 200, y: 325))
+canvas.drawLine(from: Point(x: 150, y: 300), to: Point(x: 100, y: 325))
+canvas.drawLine(from: Point(x: 100, y: 325), to: Point(x: 100, y: 275))
+canvas.drawLine(from: Point(x: 200, y: 325), to: Point(x: 200, y: 275))
+canvas.drawLine(from: Point(x: 150, y: 350), to: Point(x: 200, y: 325))
+canvas.drawLine(from: Point(x: 150, y: 350), to: Point(x: 100, y: 325))
+canvas.drawLine(from: Point(x: 250, y: 250), to: Point(x: 250, y: 200))
+canvas.drawLine(from: Point(x: 250, y: 200), to: Point(x: 200, y: 175))
+canvas.drawLine(from: Point(x: 100, y: 175), to: Point(x: 50, y: 200))
+canvas.drawLine(from: Point(x: 50, y: 200), to: Point(x: 50, y: 250))
+canvas.drawAxes(withScale: true)
