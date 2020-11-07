3ab247bb5f2b729f7a85268274aa0ec8952def2f
matt collyer <mcollyer@lcs.on.ca>
2020-11-07 11:07:30 -0500

    randomly sets initial direction of circle

--- Animation/LineFromCircles.swift
+++ Animation/LineFromCircles.swift
@@ -17,8 +17,7 @@ class LineFromCircles: NSObject, Sketchable {
     var canvas: Canvas
     
     // Make a small and large circle
-    //    var small: MovingCircle
-    //    var large: MovingCircle
+   
     var circles: [MovingCircle] = [] // Empty list (array)
     
     
@@ -29,59 +28,47 @@ class LineFromCircles: NSObject, Sketchable {
         canvas = Canvas(width: 500, height: 500)
         
         // add circle
-        //        for _ in stride(from: 1, to: 2, by: 1) {
-        //
-        //        }
-        for _ in 1...25 {
+        for _ in 1...50 {
+            
+            //randomly pick horizontal direction
+           var dx = 1
+            if Bool.random() == true {
+                dx *= -1
+            }
+            //randomly pick vertical direction
+                      var dy = 1
+                       if Bool.random() == true {
+                           dy *= -1
+                       }
+                       
+            // create new circle
             var newCircle = MovingCircle(x: Int.random(in: 0...canvas.width),
                                          y: Int.random(in: 0...canvas.height),
-                                         dx: 1,
-                                         dy: -1,
-                                         diameter: 200)
+                                         dx: dx,
+                                         dy: dy,
+                                         diameter: 60)
             
             // now add the new circle to the list
             circles.append(newCircle)
             
         }
-        // set up small and large circle
-        
-        
-        // set up small and large circle
-        
-        
         canvas.drawShapesWithBorders = true
         canvas.borderColor = Color.black
         
     }
-    
-    // This function runs repeatedly, forever, to create the animated effect
-    
     // This function runs repeatedly, forever, to create the animated effect
     func draw() {
-        
-        // var int = Int.random(in: 1...500)
-        // var int2 = Int.random(in: 1...500)
-        // let rainbow = Color(hue: int2, saturation: int, brightness: int, alpha: int)
-        
-        // canvas.fillColor = Color.black
-        
         canvas.drawShapesWithFill = true
         canvas.fillColor = Color.white
         canvas.drawRectangle(at: Point(x: 0, y: 0), width: 500, height: 500)
         canvas.defaultBorderWidth = 7
+       
         // update each circls position on the canvas
         for i in 0...circles.count - 1 {
             circles[i].update(on: canvas)
         }
         
         // Check whether the circles overlap
-        //       circles[0].drawLineWhenOverLappingWidth(other: circles[1], on: canvas)
-        //        circles[0].drawLineWhenOverLappingWidth(other: circles[2], on: canvas)
-        //        circles[0].drawLineWhenOverLappingWidth(other: circles[3], on: canvas)
-        //        circles[1].drawLineWhenOverLappingWidth(other: circles[2], on: canvas)
-        //         circles[1].drawLineWhenOverLappingWidth(other: circles[3], on: canvas)
-        //        circles[2].drawLineWhenOverLappingWidth(other: circles[3], on: canvas)
-        
         for i in stride(from: 0, through: circles.count - 2, by: 1) {
             
             for j in stride(from: i + 1, through: circles.count - 1, by: 1) {
